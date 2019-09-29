1c3b7190ac8cb581767e965e25f4c9ea84cb7fa1
Thomas Balzer <thomas.balzer@gmail.com>
2019-09-28 23:46:13 -0400

    Update to Session #3 and Material for #4
    Law of Large Number exampled extended to cover histograms and equivalent example for C.L.T. Importance Sampling for Exponential Distribution.

--- mmf_2019_3_LLN.py
+++ mmf_2019_3_LLN.py
@@ -8,7 +8,7 @@ import numpy as np
 
 import core_math_utilities as dist
 import plot_utilities as pu
-
+import matplotlib.pyplot as plt
 
 ###########
 ##
@@ -43,12 +43,68 @@ def binomial_lln(sample_size, p):
     mp = pu.PlotUtilities("Cumulative Average", 'x', 'Average')
     mp.multiPlot(x_ax, y_ax)
 
+def binomial_lln_hist(sample_size, repeats, p):
+
+    ### plot histogram of Average value of normalised Binomial Distributions
+
+    sample_value = [0.] * repeats
+    num_bins = 35
+    for i in range(repeats):
+
+        ######
+        ## Step 1 - create sample of independent uniform random variables
+
+        lower_bound = 0.
+        upper_bound = 1.
+        uni_sample = np.random.uniform(lower_bound, upper_bound, sample_size)
+
+        ######
+        ## Step 2 - transform them to $B(1,p)$ distribution
+
+        sample = [dist.binomial_inverse_cdf(p,u) for u in uni_sample]
+        sample_value[i] = (sum(sample) - sample_size * p) / np.sqrt(p * (1-p)) / sample_size
+
+    mp = pu.PlotUtilities("Histogram of Normalised Binomial Average For Sample of Size={0}".format(sample_size), 'Outcome', 'Rel. Occurrence')
+    mp.plotHistogram(sample_value, num_bins)
+
+def binomial_clt_hist(sample_size, repeats, p):
+
+    ### plot histogram of Average value of normalised Binomial Distributions
+
+    sample_value = [0.] * repeats
+    num_bins = 35
+    for i in range(repeats):
+
+        ######
+        ## Step 1 - create sample of independent uniform random variables
+
+        lower_bound = 0.
+        upper_bound = 1.
+        uni_sample = np.random.uniform(lower_bound, upper_bound, sample_size)
+
+        ######
+        ## Step 2 - transform them to $B(1,p)$ distribution
+
+        sample = [dist.binomial_inverse_cdf(p,u) for u in uni_sample]
+        sample_value[i] = (sum(sample) - sample_size * p) / np.sqrt(sample_size * p * (1-p))
+
+    plt.xlabel('Outcome')
+    plt.ylabel('Relative Occurrence')
+    plt.title("Histogram of Normalised Binomial Average For Sample of Size={0}".format(sample_size))
+
+    _n, bins, _hist = plt.hist(sample_value, num_bins, normed=True, facecolor='green', alpha=0.75)
+
+    y = [dist.standard_normal_pdf(b) for b in bins]
+    plt.plot(bins, y, 'r--')
+    plt.show()
+    plt.close()
 
 if __name__ == '__main__':
 
-    sz = 1500
+    sz = 10000
     p = .75
-    binomial_lln(sz, p)
-
+    # binomial_lln(sz, p)
+    repeats = 2000
+    binomial_clt_hist(sz, repeats, p)
 
 
---
+++ mmf_2019_4_importance sampling.py
@@ -0,0 +1,114 @@
+################
+## Author: Thomas Balzer
+## (c) 2019
+## Material for MMF Stochastic Analysis - Fall 2019
+################
+
+import numpy as np
+
+import core_math_utilities as dist
+import plot_utilities as pu
+
+
+class OptionPayout():
+
+    def __init__(self, strike):
+        self.strike = strike
+
+    def value(self, x):
+        return 0.
+
+class OptionPayoutCall(OptionPayout):
+
+    def __init__(self, strike):
+        self.strike = strike
+
+    def value(self, x):
+        return max(x - strike, 0.)
+
+
+class OptionPayoutDigital(OptionPayout):
+
+    def __init__(self, strike):
+        self.strike = strike
+
+    def value(self, x):
+        if (x > strike):
+            return 1.
+        else:
+            return 0.
+
+
+def exponential_importance_sampling(_lambda, shift, strike, option_payout):
+
+    ## we evaluate a payout of either $P(X > K)$ or $E(X-K)^+)$ for an exponential distribution with a given _lambda and a shifted model where
+    ## the shift is set to bring the mean of the exponentially tilted distribution to the strike that is considered
+
+    repeats = 500
+
+    sample_post_is = [0.] * 2
+    sample_post_is[0] = [0] * repeats  # this is be the sample WITHOUT I.S. applied
+    sample_post_is[1] = [0] * repeats  # this is be the sample WITH I.S. applied
+
+    for z in range(repeats):
+        ## we are sampling sz times in each iteration
+
+        sz = 5000
+
+        lower_bound = 0.
+        upper_bound = 1.
+
+        ## we create a uniform sample first;
+
+        uni_sample = np.random.uniform(lower_bound, upper_bound, sz)
+
+        #######
+        ### transform the uniform sample to exponentials with two different shifts
+        #######
+
+        sample_exp = [dist.exponential_inverse_cdf(_lambda, u) for u in uni_sample]
+        sample_exp_shift = [dist.exponential_inverse_cdf(_lambda + shift, u) for u in uni_sample]
+
+        ### evaluate the payout
+
+        payout_non_is = 0
+        payout_is = 0
+
+        c = (_lambda + shift) / _lambda
+
+        payout_non_is = sum([option_payout.value(s_e) for s_e in sample_exp]) / sz
+        payout_is = sum([np.exp(shift * s_e_s) / c * option_payout.value(s_e_s) for s_e_s in sample_exp_shift]) / sz
+
+        sample_post_is[0][z] = payout_non_is
+        sample_post_is[1][z] = payout_is
+
+    #######
+    ### prepare and show plot
+    ###
+    num_bins = 50
+
+    ### this is the exact result
+    p = np.exp(-strike * _lambda)
+
+    print (np.var(sample_post_is[0]))
+    print (np.var(sample_post_is[1]))
+
+    colors = ['green', 'blue']
+    _title = "Option Payout with and w/o I.S."
+    #    if (digitalPayout):
+    #       _title =  "Digital Payout (Value = {0}) with and w/o I.S.".format(p)
+
+    mp = pu.PlotUtilities(_title, 'Outcome', 'Rel. Occurrence')
+    mp.plotMultiHistogram(sample_post_is, num_bins, colors)
+
+
+if __name__ == '__main__':
+    intensity = 0.275
+    strike = 15
+    # shift = 1. / strike - intensity
+    shift = 1. / strike - np.sqrt(1. / (strike * strike) + intensity * intensity)
+
+    p = OptionPayoutDigital(strike)
+    # p = OptionPayoutCall(strike)
+    #    digitalPayout = False
+    exponential_importance_sampling(intensity, shift, strike, p)
