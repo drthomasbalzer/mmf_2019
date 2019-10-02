aadd66d0b7d0af8b6fed6871ba837c2c7c634688
Thomas Balzer <thomas.balzer@gmail.com>
2019-10-02 05:40:12 -0400

    Update mmf_2019_4_importance sampling.py
    Update to Importance Sampling example to include plots of sample variance.

--- mmf_2019_4_importance sampling.py
+++ mmf_2019_4_importance sampling.py
@@ -101,14 +101,47 @@ def exponential_importance_sampling(_lambda, shift, strike, option_payout):
     mp = pu.PlotUtilities(_title, 'Outcome', 'Rel. Occurrence')
     mp.plotMultiHistogram(sample_post_is, num_bins, colors)
 
+def variance_normal_digital(strike):
+
+    x_label = 'shift'
+    y_label = 'Variance'
+    chart_title = 'Sample Variance Under Exponential Tilting (Normal)'
+    min_val = strike - 2.
+    max_val = strike + 2.
+    steps = 1000
+    step = (max_val - min_val) / steps
+    x_ax = [min_val + step * k for k in range(steps)]
+    y_ax = [np.exp(x * x) * dist.standard_normal_cdf(-strike - x) for x in x_ax]
+
+    mp = pu.PlotUtilities(chart_title, x_label, y_label)
+    mp.multiPlot(x_ax, [y_ax])
+
+def variance_exponential_digital(strike, intensity):
+
+    _l = intensity
+    x_label = 'shift'
+    y_label = 'Variance'
+    chart_title = 'Sample Variance Under Exponential Tilting (Exponential)'
+    min_val = 0. # 1./strike * 0.2
+    max_val = intensity - 0.001
+    steps = 1000
+    step = (max_val - min_val) / steps
+    x_ax = [min_val + step * k for k in range(steps)]
+    y_ax = [np.exp(-(_l + x) * strike) * _l * _l / (_l * _l - x * x) for x in x_ax]
+
+    mp = pu.PlotUtilities(chart_title, x_label, y_label)
+    mp.multiPlot(x_ax, [y_ax])
 
 if __name__ == '__main__':
     intensity = 0.275
     strike = 15
     # shift = 1. / strike - intensity
     shift = 1. / strike - np.sqrt(1. / (strike * strike) + intensity * intensity)
-
+    print (shift)
     p = OptionPayoutDigital(strike)
     # p = OptionPayoutCall(strike)
     #    digitalPayout = False
-    exponential_importance_sampling(intensity, shift, strike, p)
+    # exponential_importance_sampling(intensity, shift, strike, p)
+    variance_exponential_digital(strike, intensity)
+
+    # variance_normal_digital(2.)
